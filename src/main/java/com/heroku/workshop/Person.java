76c4b7ee2114fe42fab282e9b933bb287a6b3e3c
nacions <stephenmark.nacionales@toyota.com>
2018-04-11 11:10:23 -0700

    sausage-making

--- src/main/java/com/heroku/workshop/Person.java
+++ src/main/java/com/heroku/workshop/Person.java
@@ -1,9 +1,14 @@
 package com.heroku.workshop;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String firstName;
     private String lastName;
